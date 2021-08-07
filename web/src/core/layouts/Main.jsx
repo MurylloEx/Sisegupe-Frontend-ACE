5d32f35490016acbd08ee5de1de2a767020679b7
Luiz Gustavo <luiz.matias@upe.br>
2021-08-07 16:22:04 -0300

    feat(core/layouts/main): get the pageTitle from children type properties

--- web/src/core/layouts/Main.jsx
+++ web/src/core/layouts/Main.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import Head from "next/head";
 import { Grid, GridItem } from "@chakra-ui/react";
 
 import { useTheme } from "core/hooks";
 
 import { UserWidget } from "core/components";
-
+/**
+ *
+ * @param {{children: React.ReactNode}} props
+ * @returns JSX.Element
+ */
 const MainLayout = ({ children }) => {
   const { colors } = useTheme();
+  const { type } = children;
+  const { pageTitle } = type ?? {};
 
   return (
     <>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
       <Grid
         templateColumns="1fr 2fr"
         bg={colors.background}
