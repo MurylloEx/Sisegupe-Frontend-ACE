881927263de77a5c8141f1e3b8895ef00648bc00
Luiz Gustavo <luiz.matias@upe.br>
2021-08-06 22:42:57 -0300

    feat(pages): populate pages with initial content

--- web/src/pages/home/projects.jsx
+++ web/src/pages/home/projects.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Grid, Text } from "@chakra-ui/react";
+import { useTheme } from "core/hooks";
 
 const Projects = () => {
+  const { colors } = useTheme();
+
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <Grid
+      bg={colors.background}
+      width="-moz-max-content"
+      height="-moz-max-content"
+    >
+      <Text>Projects</Text>
+    </Grid>
   );
 };
 
