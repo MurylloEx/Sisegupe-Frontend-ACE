d52d95b363828633621892414ae50388f705ebb6
Luiz Gustavo <luiz.matias@upe.br>
2021-08-07 11:59:29 -0300

    feat(core/components): create a user widget

---
+++ web/src/core/components/UserWidget.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Grid,
+  GridItem,
+  Avatar,
+  Flex,
+  Text,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { useTheme } from "core/hooks";
+
+const UserWidget = () => {
+  const { colors } = useTheme();
+
+  return (
+    <GridItem width="-moz-max-content">
+      <Flex flex={1} flexDirection="column" alignItems="center" mt="48" p={4}>
+        <Avatar
+          name="Luiz Gustavo"
+          size="2xl"
+          mb={4}
+          bg={colors.red}
+          color={colors.white}
+        />
+        <Text fontSize="2xl">Luiz Gustavo</Text>
+        <Box mb="10" />
+        <Button
+          bg={colors.blue}
+          color={colors.white}
+          mb={4}
+          width="50%"
+          size="lg"
+        >
+          Meu Perfil
+        </Button>
+        <Button bg={colors.blue} color={colors.white} size="lg" width="50%">
+          Meus Projetos
+        </Button>
+      </Flex>
+    </GridItem>
+  );
+};
+
+export default UserWidget;
