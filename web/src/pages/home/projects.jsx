d73990393fac0cc8417a03230fb2f241ba7e3f88
Luiz Gustavo <luiz.matias@upe.br>
2021-08-18 18:55:21 -0300

    feat(pages/home/projects): create new page

--- web/src/pages/home/projects.jsx
+++ web/src/pages/home/projects.jsx
@@ -1,27 +1,14 @@
 import React from "react";
-import {
-  Grid,
-  GridItem,
-  Avatar,
-  Flex,
-  Text,
-  Button,
-  Box,
-  Stack,
-} from "@chakra-ui/react";
 
-import { useTheme } from "core/hooks";
+import { ProjectsWidget } from "core/components";
+import { MockedData } from "core/utils";
+
+const { DATA } = MockedData;
 
 const Projects = () => {
-  const { colors } = useTheme();
-
-  return (
-    <Grid templateRows="0.5fr 2fr">
-      <Text fontSize="2xl">Tela onde ficará os projetos</Text>
-    </Grid>
-  );
+  return <ProjectsWidget projects={DATA} />;
 };
 
-Projects.pageTitle = "Projetos";
+Projects.configs = { pageTitle: "Projetos", hasUserWidget: false };
 
 export default Projects;
