55df66dc6013abd8daa1dca333db8f326f9cfd22
Luiz Gustavo <luiz.matias@upe.br>
2021-08-18 23:37:47 -0300

    feat(pages/user/my-profile): add default values to text inputs fields

--- web/src/pages/user/my-profile.jsx
+++ web/src/pages/user/my-profile.jsx
@@ -3,31 +3,45 @@ import { Box, Flex, Stack } from "@chakra-ui/react";
 
 import { Button, TextInput } from "core/components";
 
+const DEFAULT_VALUES = {
+  name: "Luiz Gustavo",
+  email: "luiz.matias@upe.br",
+  telefone: "81 99999-9999",
+};
+
 const MyProfile = () => {
   return (
     <Flex height="100%" flexDir="column" pt="48">
       <Stack spacing="10">
         <TextInput
+          defaultValue={DEFAULT_VALUES.name}
           variant="outline"
           borderWidth={3}
           p={5}
-          hasEditableButton
           label="Nome completo"
+          hasEditableButton
+          isDisabled
         />
         <TextInput
+          defaultValue={DEFAULT_VALUES.email}
+          type="email"
           variant="outline"
           borderWidth={3}
           p={5}
-          hasEditableButton
           label="Email"
+          hasEditableButton
+          isDisabled
         />
 
         <TextInput
+          defaultValue={DEFAULT_VALUES.telefone}
+          type="tel"
           variant="outline"
           borderWidth={3}
           p={5}
           hasEditableButton
           label="Telefone"
+          isDisabled
         />
       </Stack>
       <Box height="10" />
