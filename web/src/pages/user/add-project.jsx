8fa65f4805d2de1e227abc3c13974cc9a0a662ee
Luiz Gustavo <luiz.matias@upe.br>
2021-08-19 22:56:26 -0300

    feat(pages/user): finish user add-project page

--- web/src/pages/user/add-project.jsx
+++ web/src/pages/user/add-project.jsx
@@ -1,24 +1,58 @@
-import { Box, Stack, VStack } from "@chakra-ui/react";
-import { TextInput } from "core/components";
 import React from "react";
+import { Stack } from "@chakra-ui/react";
+import DescriptionIcon from "@material-ui/icons/Description";
+
+import { Button, Select, TextInput } from "core/components";
+
+const COURSES_DATA = ["Engenharia de software", "Psicologia", "Biologia"];
+
+const STATUS_DATA = ["Em andamento", "Concluído"];
 
 const AddProject = () => {
   return (
-    <Stack direction="column" spacing="10" width="100%">
-      <TextInput variant="outline" borderWidth={3} p={5} label="Nome(s)" />
-      <TextInput variant="outline" borderWidth={3} p={5} label="Título" />
+    <Stack direction="column" spacing="10">
+      <TextInput
+        variant="outline"
+        borderWidth={3}
+        p={5}
+        label="Nome(s)"
+        placeholder="Digite aqui o seu nome"
+      />
+      <TextInput
+        variant="outline"
+        borderWidth={3}
+        p={5}
+        label="Título"
+        placeholder="Digite aqui o título do seu projeto"
+      />
       <TextInput
         variant="outline"
         borderWidth={3}
         p={5}
         label="Descrição"
         isTextarea
-        rows="4"
+        placeholder="Adicione uma breve descrição sobre o seu projeto"
+        rows="10"
       />
+      <Select
+        label="Cursos"
+        placeholder="Selecione o curso que você realizou na UPE"
+        items={COURSES_DATA}
+      />
+      <Select
+        label="Status"
+        placeholder="Selecione o status do seu projeto"
+        items={STATUS_DATA}
+      />
+      <Button.Outlined icon={(props) => <DescriptionIcon {...props} />}>
+        Adicionar arquivo
+      </Button.Outlined>
+
+      <Button bg="success">Confirmar projeto</Button>
     </Stack>
   );
 };
 
-AddProject.configs = { pageTitle: "Adicionar projeto", hasUserWidget: false };
+AddProject.configs = { pageTitle: "Adicionar projeto", hasUserWidget: true };
 
 export default AddProject;
