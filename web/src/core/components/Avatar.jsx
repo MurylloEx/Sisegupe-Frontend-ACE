52a584d4b706fdec12b832459e278df4a806bb02
Luiz Gustavo <luiz.matias@upe.br>
2021-08-20 12:28:09 -0300

    feat(core/components/avatar): add the possibility to use random bg colors and wrap `ChakraAvatar` and `Text` in a `VStack` component

--- web/src/core/components/Avatar.jsx
+++ web/src/core/components/Avatar.jsx
@@ -1,24 +1,21 @@
-import { Avatar as ChakraAvatar, Text } from "@chakra-ui/react";
+import { Avatar as ChakraAvatar, VStack, Text } from "@chakra-ui/react";
 import { useTheme } from "core/hooks";
 import React from "react";
 
-const Avatar = ({ name, fontSize }) => {
+const Avatar = ({ name, fontSize, size = "2xl", isRandomBgColor }) => {
   const { colors } = useTheme();
 
   if (!name) {
     throw Error("Name property is necessary to render the Avatar");
   }
+
+  const bg = isRandomBgColor ? null : colors.secondary;
+
   return (
-    <>
-      <ChakraAvatar
-        name={name}
-        size="2xl"
-        mb={4}
-        bg={colors.secondary}
-        color={colors.white}
-      />
+    <VStack width="50%">
+      <ChakraAvatar name={name} size={size} bg={bg} color={colors.white} />
       <Text fontSize={fontSize ?? "2xl"}>{name}</Text>
-    </>
+    </VStack>
   );
 };
 
