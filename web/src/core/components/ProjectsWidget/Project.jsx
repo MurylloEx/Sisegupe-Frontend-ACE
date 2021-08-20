d85fce6333c35c9dd63a4a0223d0c72b1b6811a9
Luiz Gustavo <luiz.matias@upe.br>
2021-08-19 23:00:01 -0300

    feat(core/components/projectswidget): add commentary modal

--- web/src/core/components/ProjectsWidget/Project.jsx
+++ web/src/core/components/ProjectsWidget/Project.jsx
@@ -1,54 +1,62 @@
 import React from "react";
-import { Flex, HStack } from "@chakra-ui/react";
+import { HStack, useDisclosure } from "@chakra-ui/react";
+import ForumIcon from "@material-ui/icons/Forum";
+
+import { useTheme } from "core/hooks";
+import { CommentaryModal } from "core/modals";
+
 import { Card } from "../Card";
 import { Tag } from "../Tag";
 import { Button } from "../Button";
-import ForumIcon from "@material-ui/icons/Forum";
-import { useTheme } from "core/hooks";
 
 const Project = ({ project, ...props }) => {
   const { status, title, content } = project;
   const { colors } = useTheme();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   return (
-    <Card
-      borderWidth={3}
-      header={() => (
-        <HStack justify="space-between" px={4}>
-          <Card.TextHeader
-            fontColor="primary"
-            textAlign="left"
-            fontSize="40"
-            p={0}
-            bold
-          >
-            {title}
-          </Card.TextHeader>
-          <Tag tagType={status} />
-        </HStack>
-      )}
-      footer={() => (
-        <HStack justify="flex-end" m={4} spacing="10">
-          <Button.Icon
-            icon={
-              <ForumIcon fontSize="large" style={{ color: colors.primary }} />
-            }
-          />
-          <Button width="20%">Saiba mais</Button>
-        </HStack>
-      )}
-      mb={4}
-      {...props}
-    >
-      <Card.TextBody
-        fontSize={20}
-        textAlign="left"
-        color="grayMedium"
-        bold={false}
+    <>
+      <Card
+        borderWidth={3}
+        header={() => (
+          <HStack justify="space-between" px={4}>
+            <Card.TextHeader
+              fontColor="primary"
+              textAlign="left"
+              fontSize="40"
+              p={0}
+              bold
+            >
+              {title}
+            </Card.TextHeader>
+            <Tag tagType={status} />
+          </HStack>
+        )}
+        footer={() => (
+          <HStack justify="flex-end" m={4} spacing="10">
+            <Button.Icon
+              icon={
+                <ForumIcon fontSize="large" style={{ color: colors.primary }} />
+              }
+              onClick={onOpen}
+            />
+            <Button width="20%">Saiba mais</Button>
+          </HStack>
+        )}
+        mb={4}
+        {...props}
       >
-        {content}
-      </Card.TextBody>
-    </Card>
+        <Card.TextBody
+          fontSize={20}
+          textAlign="left"
+          color="grayMedium"
+          bold={false}
+        >
+          {content}
+        </Card.TextBody>
+      </Card>
+      <CommentaryModal isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
