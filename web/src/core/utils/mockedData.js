79e18819ee0458b15f8113aff77b24e1e7bbcfa8
Luiz Gustavo <luiz.matias@upe.br>
2021-08-21 17:23:19 -0300

    feat(core/utils): add more fields to mocked data

--- web/src/core/utils/mockedData.js
+++ web/src/core/utils/mockedData.js
@@ -1,20 +1,38 @@
 export const DATA = [
   {
     title: "Projeto 1",
+    members: "Luiz Gustavo",
+    course: "Engenharia de Software",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "Concluído",
+    id: 1,
   },
   {
     title: "Projeto 2",
+    members: "Lucas Henrique",
+    course: "Psicologia",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "Em andamento",
+    id: 2,
   },
   {
     title: "Projeto 3",
+    members: "Kelvin Vasconcelos",
+    course: "Medicina",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     status: "Em andamento",
+    id: 3,
+  },
+  {
+    title: "Projeto 4",
+    members: "Muryllo Pimenta",
+    course: "História",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "Em andamento",
+    id: 3,
   },
 ];
