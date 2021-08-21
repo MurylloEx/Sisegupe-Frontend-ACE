a1a348a3933efd507ef1ffdc23479f0e065435c2
Luiz Gustavo <luiz.matias@upe.br>
2021-08-21 17:24:06 -0300

    feat(core/components/navbar): add logout button and remove useTheme hook

--- web/src/core/components/Navbar.jsx
+++ web/src/core/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import NextLink from "next/link";
 import { Box, Flex, HStack, Link, Icon } from "@chakra-ui/react";
-import { Home, Info, ShowChart, PersonAdd } from "@material-ui/icons";
+import { Home, Info, ShowChart, ExitToApp } from "@material-ui/icons";
 
 import { Constants } from "core/utils";
 
-import { useTheme } from "../hooks";
 import Logo from "./Logo";
+import { Button } from "./Button";
 
 const PAGES = [
   { link: "/", linkName: "Inicio", icon: Home },
@@ -13,9 +13,9 @@ const PAGES = [
   { link: Constants.GitHubRepositoryLink, linkName: "Sobre", icon: Info },
 ];
 
-const NavLink = ({ icon, linkName, colors, link }) => (
+const NavLink = ({ icon, linkName, link }) => (
   <NextLink href={link} passHref>
-    <Link px={2} py={1} color={colors.white} rounded={"md"}>
+    <Link px={2} py={1} color="white" rounded={"md"}>
       <Icon as={icon} mx={2} />
       {linkName}
     </Link>
@@ -23,20 +23,12 @@ const NavLink = ({ icon, linkName, colors, link }) => (
 );
 
 const Navbar = () => {
-  const { colors } = useTheme();
-
   const renderItem = ({ linkName, icon, link }) => (
-    <NavLink
-      key={linkName}
-      icon={icon}
-      linkName={linkName}
-      colors={colors}
-      link={link}
-    />
+    <NavLink key={linkName} icon={icon} linkName={linkName} link={link} />
   );
 
   return (
-    <Box bg={colors.primary} px={4}>
+    <Box bg="primary" px={4}>
       <Flex alignItems="center" justifyContent="space-between">
         <Flex alignItems="center" justifyContent="flex-start" m={4}>
           <Logo />
@@ -44,6 +36,7 @@ const Navbar = () => {
         <Flex h={16} alignItems="center" justifyContent="flex-end">
           <HStack spacing={8} alignItems="center">
             {PAGES.map(renderItem)}
+            {/* <Button.Icon icon={<ExitToApp style={{ color: "white" }} />} /> */}
           </HStack>
         </Flex>
       </Flex>
