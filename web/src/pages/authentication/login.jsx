1a8a2a61310d9a8b0fccbf67061511f38d041d61
Luiz Gustavo <luiz.matias@upe.br>
2021-08-21 12:39:02 -0300

    feat(pages/authentication/login): create login page

--- web/src/pages/authentication/login.jsx
+++ web/src/pages/authentication/login.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { Email, Lock } from "@material-ui/icons";
+import { Stack, Text } from "@chakra-ui/react";
+import { Button, TextInput } from "core/components";
 
 const Login = () => {
-  return <div />;
+  const navigation = useRouter();
+
+  const onClickNavigateToSignUpScreen = () => {
+    navigation.push("/authentication/sign-up");
+  };
+
+  return (
+    <Stack
+      width="100%"
+      flexDir="column"
+      justifyItems="center"
+      mx="30%"
+      spacing="10"
+      fontWeight="bold"
+    >
+      <Text textAlign="center" fontSize="2xl">
+        Login
+      </Text>
+      <TextInput
+        variant="outline"
+        borderWidth={3}
+        p={5}
+        placeholder="E-mail"
+        leftIcon={<Email />}
+      />
+
+      <TextInput
+        flex={3}
+        width="max"
+        variant="outline"
+        borderWidth={3}
+        p={5}
+        placeholder="Senha"
+        type="password"
+        leftIcon={<Lock />}
+      />
+      <Stack>
+        <Button bg="success" disabled>
+          Entrar
+        </Button>
+        <Button onClick={onClickNavigateToSignUpScreen}>
+          Realizar cadastro
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 Login.configs = { pageTitle: "Login", hasUserWidget: false };
