edc4d149c97a86e559aebb9ca1c5237f8807dfa9
Luiz Gustavo <luiz.matias@upe.br>
2021-08-25 21:18:18 -0300

    feat(core/modals): integrate login and sign-up services

--- web/src/core/modals/LoginModal.jsx
+++ web/src/core/modals/LoginModal.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Email, Lock } from "@material-ui/icons";
-import { CheckboxGroup, HStack, Stack, Text } from "@chakra-ui/react";
-import { Button, Modal, TextInput } from "core/components";
+import { HStack, Stack, Text } from "@chakra-ui/react";
 import { Checkbox } from "@material-ui/core";
+import { useRouter } from "next/router";
+
+import { Button, Modal, TextInput } from "core/components";
+import { useLogin, useStorage, useUser } from "core/hooks";
+
+const BEARER_TOKEN = "@sisegupe/bearer-token";
 
 const LoginModal = (props) => {
   const { onClose } = props;
+  const router = useRouter();
+  const [willSaveCredentials, setWillSaveCredentials] = useState(false);
+
+  const [setItem] = useStorage();
+
+  const [{ response, ...rest }, { requestLogin }] = useLogin({
+    email: "",
+    password: "",
+  });
+
+  const [, { login }] = useUser();
+
+  const { error, isError, isLoading, isSuccess } = rest;
+
+  const onSaveCredentials = useCallback(
+    (token) => {
+      if (willSaveCredentials) {
+        setItem(BEARER_TOKEN, token);
+        return;
+      }
+      return;
+    },
+    [setItem, willSaveCredentials]
+  );
+
+  useEffect(() => {
+    if (isSuccess && response) {
+      const { name, email, password, token } = response ?? {};
+
+      login({ name, email, password, token });
+      onSaveCredentials(token);
+
+      return router.reload(window.location.pathname);
+    }
+    return;
+  }, [isSuccess, login, onSaveCredentials, response, router]);
 
   return (
     <Modal header="Faça login" size="2xl" scrollBehavior="inside" {...props}>
@@ -32,13 +73,18 @@ const LoginModal = (props) => {
             />
 
             <HStack>
-              <Checkbox />
+              <Checkbox
+                value={willSaveCredentials}
+                onChange={(e) => setWillSaveCredentials(e.target.checked)}
+              />
               <Text> Lembrar senha</Text>
             </HStack>
           </Stack>
 
           <Stack direction="row" spacing="6">
-            <Button bg="success">Entrar</Button>
+            <Button bg="success" isLoading={isLoading}>
+              Entrar
+            </Button>
             <Button onClick={onClose}>Voltar</Button>
           </Stack>
         </Stack>
--- web/src/core/modals/SignUpModal.jsx
+++ web/src/core/modals/SignUpModal.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Email, Lock, Person } from "@material-ui/icons";
 import { Stack } from "@chakra-ui/react";
 import { Button, Modal, TextInput } from "core/components";
+import { useSignUp } from "core/hooks";
+import router from "next/router";
 
 const SignUpModal = (props) => {
   const { onClose } = props;
+  const [{ response, ...rest }, { requestSignUp }] = useSignUp({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  const { error, isError, isLoading, isSuccess } = rest;
+
+  useEffect(() => {
+    if (isSuccess) {
+      return router.reload(window.location.pathname);
+    }
+  }, [isSuccess]);
 
   return (
     <Modal
@@ -53,7 +68,9 @@ const SignUpModal = (props) => {
         </Stack>
 
         <Stack direction="row" spacing="6">
-          <Button bg="success">Cadastrar</Button>
+          <Button bg="success" isLoading={isLoading}>
+            Cadastrar
+          </Button>
           <Button onClick={onClose}>Voltar</Button>
         </Stack>
       </Stack>
