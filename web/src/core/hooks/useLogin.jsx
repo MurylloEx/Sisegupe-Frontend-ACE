8ead23f596b71314cf82f1f5fff83c348c626954
Luiz Gustavo <luiz.matias@upe.br>
2021-08-25 21:16:57 -0300

    feat(core/hooks): create login, sign up and to storage hooks

---
+++ web/src/core/hooks/useLogin.jsx
@@ -0,0 +1,20 @@
+import { useCallback } from "react";
+import usePostRequest from "./usePostRequest";
+
+const LOGIN_ENDPOINT = "";
+
+const useLogin = ({ email, password }) => {
+  const {
+    mutate: doLogin,
+    data: response,
+    ...rest
+  } = usePostRequest(LOGIN_ENDPOINT);
+
+  const requestLogin = useCallback(() => {
+    doLogin({ email, password });
+  }, [email, password, doLogin]);
+
+  return [{ response, ...rest }, { requestLogin }];
+};
+
+export default useLogin;
