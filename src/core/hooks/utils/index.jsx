3cef3bd49757373ec2ff6a09d83a1f53ce6ed1e1
Luiz Gustavo <luiz.matias@upe.br>
2021-08-31 20:30:06 -0300

    feat(core/hook/utils): create `useToken` hook to get the user bearer token

--- src/core/hooks/utils/index.jsx
+++ src/core/hooks/utils/index.jsx
@@ -1,3 +1,4 @@
 export { default as useForm } from "./useForm";
 export { default as useReducerActions } from "./useReducerActions";
 export { default as useStorage } from "./useStorage";
+export { default as useToken } from "./useToken";
---
+++ src/core/hooks/utils/useToken.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { LOCAL_STORAGES_LOCATIONS } from "core/utils/constants";
+
+import useStorage from "./useStorage";
+import { useUser } from "../user";
+
+const useToken = () => {
+  const [actualToken, setActualToken] = useState("");
+  const [getItem] = useStorage();
+  const [{ token: loggedUserToken }] = useUser();
+
+  useEffect(() => {
+    const { token = "" } =
+      JSON.parse(getItem(LOCAL_STORAGES_LOCATIONS.USER_ACCESS_CREDENTIALS)) ??
+      {};
+
+    if (!token) {
+      return setActualToken(loggedUserToken);
+    }
+
+    return setActualToken(token);
+  }, [getItem, loggedUserToken]);
+
+  return actualToken;
+};
+
+export default useToken;
