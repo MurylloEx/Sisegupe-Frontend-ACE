a66ff4767b88e020ebb1adcab9ef4d6fd0ac2b9e
Luiz Gustavo <luiz.matias@upe.br>
2021-08-31 01:56:12 -0300

    feat(core/utils): add LOCAL_STORAGES_LOCATIONS

--- src/core/utils/constants.js
+++ src/core/utils/constants.js
@@ -1,3 +1,6 @@
 export const DISABLED_DEFAULT_BG = { bg: null };
 export const GITHUB_REPOSITORY_LINK = "https://github.com/MurylloEx/Sisegupe";
 export const ROLES = { NORMAL_USER: 1, ADMIN: 2 };
+export const LOCAL_STORAGES_LOCATIONS = {
+  USER_ACCESS_CREDENTIALS: "@sisegupe/user_access_credentials",
+};
