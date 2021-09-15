5bb20ba8e4ff28b24f5aeb95e37c065cc27d3156
Luiz Gustavo <luiz.matias@upe.br>
2021-09-15 19:50:44 -0300

    feat(pages/index): fix 'Notícias em destaque' aligment

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Grid, GridItem, Flex, HStack, Spinner, Text, Box } from "@chakra-ui/react";
+import {
+  Grid,
+  GridItem,
+  Flex,
+  HStack,
+  Spinner,
+  Text,
+  Box,
+} from "@chakra-ui/react";
 
 import { Carousel, Card } from "core/components";
 import { ROLES } from "core/utils/constants";
@@ -7,21 +15,45 @@ import { useGetRequest } from "core/hooks";
 
 const CARD_HEADER_DEFAULT = 40;
 
+const AmountInfosCard = ({ title, content }) => {
+  const renderTextBodyContent = (content) => {
+    if (!content) {
+      return <Spinner size="md" />;
+    }
+
+    return content;
+  };
+
+  return (
+    <Card
+      header={() => (
+        <Card.TextHeader fontColor="grayMedium" bold>
+          {title}
+        </Card.TextHeader>
+      )}
+      mb={4}
+      height={CARD_HEADER_DEFAULT}
+    >
+      <Card.TextBody>{renderTextBodyContent(content)}</Card.TextBody>
+    </Card>
+  );
+};
+
 const HomePage = () => {
   const { data: response } = useGetRequest("/graphics/projects");
   const { data } = response ?? {};
   const { data: projectsInfos } = data ?? {};
-  const { AllProjects, InProgress, Finished } = projectsInfos ?? {};
-
-  const Loading = () => {
-    return <Spinner size="md" />;
-  };
+  const {
+    AllProjects: allProjectsAmount,
+    InProgress: inProgressProjectsAmount,
+    Finished: finishedProjectsAmount,
+  } = projectsInfos ?? {};
 
   return (
     <Grid templateRows="0.5fr 2fr">
       <GridItem>
         <Flex flex={1} flexDirection="column" justifyItems="center">
-          <Text align="center" fontSize="3xl" fontWeight="bold" mb="4">
+          <Text fontSize="3xl" fontWeight="bold" mb="4">
             Notícias em destaque
           </Text>
           <Carousel.News height="200" />
@@ -29,36 +61,15 @@ const HomePage = () => {
       </GridItem>
       <GridItem py="2">
         <HStack>
-          <Card
-            header={() => (
-              <Card.TextHeader fontColor="grayMedium" bold>
-                Todos
-              </Card.TextHeader>
-            )}
-            mb={4}
-            height={CARD_HEADER_DEFAULT}>
-            <Card.TextBody> {AllProjects ?? <Loading />} </Card.TextBody>
-          </Card>
-          <Card
-            header={() => (
-              <Card.TextHeader fontColor="grayMedium" bold>
-                Em andamento
-              </Card.TextHeader>
-            )}
-            mb={4}
-            height={CARD_HEADER_DEFAULT}>
-            <Card.TextBody> {InProgress ?? <Loading />} </Card.TextBody>
-          </Card>
-          <Card
-            header={() => (
-              <Card.TextHeader fontColor="grayMedium" bold>
-                Concluídos
-              </Card.TextHeader>
-            )}
-            mb={4}
-            height={CARD_HEADER_DEFAULT}>
-            <Card.TextBody> {Finished ?? <Loading />} </Card.TextBody>
-          </Card>
+          <AmountInfosCard title="Todos" content={allProjectsAmount} />
+          <AmountInfosCard
+            title="Em andamento"
+            content={inProgressProjectsAmount}
+          />
+          <AmountInfosCard
+            title=" Concluídos"
+            content={finishedProjectsAmount}
+          />
         </HStack>
         <Box>
           <Carousel.Charts />
